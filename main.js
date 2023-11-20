1eda56bb1584069e4e6691f2f461df111263d44c
sephsx <olorbidajoseph05@gmail.com>
2023-11-20 21:09:40 +0800

    primary done
    Todo app. this is simple project using js and can do create,read,edit,remove

--- main.js
+++ main.js
@@ -1,23 +1,54 @@
 
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded',function(){
   const form = document.querySelector('form')
   const input = document.querySelector('input')
-  const ul = document.getElementById('add');
+  const ul = document.getElementById('add')
 
-  form.addEventListener('submit', function(event){
+  form.addEventListener('submit',function(event){
     event.preventDefault()
-
     //get the value of the input field
     const inputValue = input.value
 
+    //check if the user did not put the value
+    if(inputValue.trim() == ''){
+      alert('Please enter a task before adding.')
+    }
     //create a new list item
     const li = document.createElement('li')
-    li.textContent = inputValue
+    li.style.display = 'flex'
+    li.style.alignItems = 'center'
+    //create a span for the text content
+    const span = document.createElement('span')
+    span.textContent = inputValue
+    span.style.marginRight = '100px'
 
-    //append into the input field
+    //create a edit and remove button
+    const editIcon = document.createElement('i')
+    editIcon.className = 'fas fa-edit'
+    editIcon.style.marginRight = '10px'
+    editIcon.addEventListener('click', () =>{
+      const newText = prompt('Edit your task:', span.textContent)
+      if(newText !== null){
+        span.textContent = newText
+      }
+    })
+    
+    const removeIcon = document.createElement('i')
+    removeIcon.className = 'fas fa-trash'
+    removeIcon.style.marginRight = '20px'
+    removeIcon.addEventListener('click', () =>{
+      ul.removeChild(li)
+    })
+
+    //append span and icons to the list item
+    li.appendChild(span)
+    li.appendChild(editIcon)
+    li.appendChild(removeIcon)
+
+    //append the list item to the ul
     ul.appendChild(li)
 
-    //clear the output field
+    //clear the input field
     input.value = ''
   })
 })
